5aa5f0bce21eb2d71c61fcd7caf5a355f0f77bb8
betanyeli <betangelii@gmail.com>
2020-08-22 02:50:45 -0400

    prescription screen

--- src/views/prescriptions/Prescriptions.jsx
+++ src/views/prescriptions/Prescriptions.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import {
+  Container,
+  Grid,
+
+
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Cards from './Cards'
+
+
+
+const useStyles = makeStyles((theme) => ({
+
+  cardContainer: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+    display: "flex",
+    flexDirection: "column",
+  },
+  
+  textStyles: {
+    textAlign: "initial",
+  },
+}));
 
 const Prescriptions = () => {
-    return (
-        <>
-            <h1>Recetas</h1>
-        </>
-    )
-}
+  const classes = useStyles();
+  return (
+    <>
+      <Container maxWidth="md">
+ 
+        <b className={classes.textStyles}>RECETAS Y EXÁMENES</b>
+        <Grid container spacing={4} className={classes.cardContainer}>
+            <Cards />
+        </Grid>
+      </Container>
+    </>
+  );
+};
 
 export default Prescriptions;
